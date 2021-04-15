6a91c7420c127163c32ef55fd1ca90212f3eb178
Lewis Bawden <lba@trackman.com>
2021-04-15 21:40:30 +0200

    add autoscroll and task cancelling

--- multibars.py
+++ multibars.py
@@ -1,9 +1,11 @@
 import os
+import sys
 import time
-import random
 from copy import copy
+import random
 import PyQt5.Qt as qt
-
+from tqdm import tqdm
+import multiprocessing as mp
 
 def wrapped_timer(func):
     def wrapper(*args, **kwargs):
@@ -12,6 +14,16 @@ def wrapped_timer(func):
         out = func(*args, **kwargs)
         print(f"Duration {func.__name__}: {time.time() - t0}")
         return out
+    return wrapper
+
+
+def handle_mutex_and_catch_runtime(func):
+    def wrapper(inst, *args, **kwargs):
+        locker = qt.QMutexLocker(inst.mutex)
+        try:
+            return func(inst, *args, **kwargs)
+        except RuntimeError:
+            return None
     return wrapper
 
 
@@ -26,6 +38,8 @@ class Worker(qt.QThread):
         self.kwargs = kwargs
         self.pid = kwargs.pop('pid', None)
 
+        self.setPriority(qt.QThread.HighestPriority)
+
     def run(self):
         out = self.func(*self.args, **self.kwargs)
         self.send_result.emit(self.pid, out)
@@ -33,30 +47,54 @@ class Worker(qt.QThread):
 
 
 class LabeledProgressBar(qt.QProgressBar):
+    cancelTaskSignal = qt.pyqtSignal(int)
     unit_conv = {3: 'k', 6: 'M', 9: 'G', 12: 'T'}
 
-    def __init__(self, total=100, name=" ", units_symbol="", parent=None):
+    def __init__(self, total=100, name=" ", units_symbol="", pid=None, parent=None):
         super(LabeledProgressBar, self).__init__(parent)
-        self.name = f"{name}"
-        self.progress = 0
         self.total = total
         self.units_symbol = units_symbol
+        self.pid = pid
+        self.task_name = f"{name}"
+        self.full_name = self.get_full_name(self.task_name)
 
-        self.min_update_increment = 1  # e.g. total // 500
-        self.max_update_frequency = 0  # e.g. 0.01 seconds
+        self.min_update_increment = total // 250
+        self.max_update_frequency = 0.05
         self.last_updated = self.get_time()
+        self.recent_iteration_durations = []
 
         self.total_str = self.get_formatted_number(total)
-        self.progress_str = " / ".join([self.get_formatted_number(self.progress), self.total_str])
+        self.progress_str = self.get_progress_str(0)
+        self.frequency_str = self.get_frequency_str()
 
-        self.prefix_label = qt.QLabel(self.name)
+        self.prefix_label = qt.QLabel(self.full_name)
         self.progress_label = qt.QLabel(self.progress_str)
+        self.frequency_label = qt.QLabel()
 
+        self.prefix_label.setEnabled(False)
+        self.progress_label.setEnabled(False)
+        self.frequency_label.setEnabled(False)
+
+        self.setMinimumSize(200, 20)
         self.setRange(0, total)
         self.setMouseTracking(False)
         self.setTextVisible(False)
 
         self.show()
+
+    def send_cancel_task_signal(self):
+        self.cancelTaskSignal.emit(self.pid)
+
+    def set_color_cancelled(self):
+        self.setStyleSheet("""QProgressBar::chunk{background-color : #AAAAAA;}""")
+
+    def mousePressEvent(self, a0: qt.QMouseEvent):
+        if a0.button() == qt.Qt.RightButton:
+            menu = qt.QMenu()
+            cancel_task_act = menu.addAction('Cancel task')
+            cancel_task_act.triggered.connect(self.send_cancel_task_signal)
+            pos = a0.globalPos()
+            menu.exec(pos)
 
     def get_formatted_number(self, value):
         factor, unit_prefix = self.get_units_prefix(value)
@@ -73,6 +111,18 @@ class LabeledProgressBar(qt.QProgressBar):
                 return 1, ''
         return factor, self.unit_conv[factor]
 
+    def get_progress_str(self, value):
+        out = ' / '.join([self.get_formatted_number(value), self.total_str])
+        return out
+
+    def get_frequency_str(self):
+        its_suffix = ' it/s'
+
+        if len(self.recent_iteration_durations) == 0:
+            return its_suffix
+
+        recent_durations = self.recent_iteration_durations[-10:]
+
     @staticmethod
     def get_time():
         return time.time()
@@ -85,27 +135,43 @@ class LabeledProgressBar(qt.QProgressBar):
     def set_value(self, value):
         self.setValue(value)
         self.last_updated = self.get_time()
-        self.progress_str = " / ".join([self.get_formatted_number(value), self.total_str])
+        self.progress_str = self.get_progress_str(value)
         self.progress_label.setText(self.progress_str)
 
+    def get_full_name(self, name):
+        if self.pid is None:
+            return name
+        else:
+            return f"Task {self.pid}: {name}"
+
     def set_name(self, name):
-        self.name = f"{name}"
-        self.prefix_label.setText(self.name)
+        self.task_name = name
+        self.full_name = self.get_full_name(name)
+        self.prefix_label.setText(self.full_name)
 
     def set_total(self, total):
         self.total = total
-        self.setRange(0, self.total)
+        progress = self.value()
+        self.setRange(progress, self.total)
         self.total_str = self.get_formatted_number(total)
-        self.progress_str = " / ".join([self.get_formatted_number(self.progress), self.total_str])
+        self.progress_str = self.get_progress_str(progress)
         self.progress_label.setText(self.progress_str)
 
 
 class ZoomingScrollArea(qt.QScrollArea):
     adjustFontSignal = qt.pyqtSignal(object)
 
-    def __init__(self):
+    def __init__(self, fontname=None, fontsize=None):
         super().__init__()
-        self.fontsize = 11
+        font = qt.QFont()
+        if fontname is None:
+            fontname = font.defaultFamily()
+        if fontsize is None:
+            fontsize = font.pointSize()
+
+        self.fontname = fontname
+        self.fontsize = fontsize
+
         self.adjust_font(1)
         self.adjustFontSignal.connect(self.adjust_font)
 
@@ -122,33 +188,22 @@ class ZoomingScrollArea(qt.QScrollArea):
         if incr < 0:
             self.fontsize = max(1, self.fontsize + incr)
         else:
-            self.fontsize = min(50, self.fontsize + incr)
-        self.setFont(qt.QFont('calibri', self.fontsize))
+            self.fontsize = min(100, self.fontsize + incr)
+        self.setFont(qt.QFont(self.fontname, self.fontsize))
 
 
 class Multibar(qt.QObject):
     allProcessesFinished = qt.pyqtSignal()
-    setValueSignal = qt.pyqtSignal(object, object)
-    setNameSignal = qt.pyqtSignal(object, object)
-    setTotalSignal = qt.pyqtSignal(object, object)
+    setNameSignal = qt.pyqtSignal(int, object)
+    setTotalSignal = qt.pyqtSignal(int, float)
+    setValueSignal = qt.pyqtSignal(int, float)
 
-    class Wrappers:
-        @staticmethod
-        def handle_mutex_and_catch_runtime(func):
-            def wrapper(inst, *args, **kwargs):
-                locker = qt.QMutexLocker(inst.mutex)
-                try:
-                    return func(inst, *args, **kwargs)
-                except RuntimeError:
-                    return None
-            return wrapper
-
-    def __init__(self, title='', batch_size=None):
+    def __init__(self, title=None, batch_size=None):
         super(Multibar, self).__init__()
+        self.title = title
+        self.batch_size = os.cpu_count() if batch_size is None else batch_size
 
         self.app = qt.QApplication([])
-
-        self.batch_size = os.cpu_count() - 1 if batch_size is None else batch_size
 
         self.pbars = dict()
         self.tasks = dict()
@@ -169,7 +224,6 @@ class Multibar(qt.QObject):
     def setup_window(self, title):
         self.layout = qt.QGridLayout()
         self.widget = qt.QWidget()
-        self.widget.setEnabled(False)
         self.widget.setLayout(self.layout)
 
         # window is a QScrollArea widget
@@ -189,6 +243,11 @@ class Multibar(qt.QObject):
         self.scroll_area.setFocus()
         self.scroll_area.show()
 
+    def scroll_down(self):
+        if len(self.running_tasks) > 0:
+            bottom = min(max(self.running_tasks) + 1, len(self.pbars) - 1)
+            self.scroll_area.ensureWidgetVisible(self.pbars[bottom], 10, 10)
+
     def add_task(self, func=callable, func_args=tuple, func_kwargs=dict(), descr='', total=1):
         """
         Add a task to be processed with the progress monitored.
@@ -198,12 +257,17 @@ class Multibar(qt.QObject):
         :param descr: Progress bar label
         :param total: Total iterations expected within the task
         """
+        if self.title is None:
+            self.title = func.__name__
+            self.scroll_area.setWindowTitle(self.title)
+
         i = len(self.pbars.keys())
         self.add_task_pbar(i, descr, total)
         self.add_task_worker(i, func, *func_args, **func_kwargs)
 
     def add_task_pbar(self, i, pbar_descr, iters_total):
-        self.pbars[i] = LabeledProgressBar(total=iters_total, name=pbar_descr, parent=self.scroll_area)
+        self.pbars[i] = LabeledProgressBar(total=iters_total, name=pbar_descr, pid=i, parent=self.scroll_area)
+        self.pbars[i].cancelTaskSignal.connect(self.confirm_remove_task)
         self.layout.addWidget(self.pbars[i].prefix_label, i, 0)
         self.layout.addWidget(self.pbars[i], i, 1)
         self.layout.addWidget(self.pbars[i].progress_label, i, 2, alignment=qt.Qt.AlignRight)
@@ -211,6 +275,21 @@ class Multibar(qt.QObject):
     def add_task_worker(self, i, apply_func, *func_args, **func_kwargs):
         updater = BarUpdater(i, self)
         self.tasks[i] = Worker(apply_func, *func_args, **func_kwargs, pid=i, pbar=updater)
+
+    def confirm_remove_task(self, pid):
+        confirm = qt.QMessageBox()
+        confirm.addButton('Cancel task', qt.QMessageBox.AcceptRole)
+        confirm.addButton('Resume task', qt.QMessageBox.RejectRole)
+        confirm.setWindowTitle('Confirm cancelling task:')
+        confirm.setText(f'Cancel task {pid}?\n {self.pbars[pid].task_name}')
+
+        def cancel():
+            print(f'Cancelling task {pid}: {self.pbars[pid].full_name}')
+            self.pbars[pid].set_color_cancelled()
+            self.end_task(pid)
+
+        confirm.accepted.connect(cancel)
+        confirm.exec()
 
     def begin_processing(self):
         self.setValueSignal.connect(self._set_pbar_value)
@@ -239,27 +318,26 @@ class Multibar(qt.QObject):
 
     def end_task(self, pid):
         if pid in self.running_tasks:
-            self.update_value(pid, self.pbars[pid].maximum())
-            self.running_tasks[pid].deleteLater()
             self.running_tasks[pid].quit()
             self.running_tasks.pop(pid)
 
     def check_all_finished(self, pid):
         self.end_task(pid)
         self.start_next()
+        self.scroll_down()
         if len(self.running_tasks) == 0:
             self.allProcessesFinished.emit()
 
-    @Wrappers.handle_mutex_and_catch_runtime
+    @handle_mutex_and_catch_runtime
     def update_value(self, pid, value):
         if self.pbars[pid].allowed_to_set_value(value):
             self.setValueSignal.emit(pid, value)
 
-    @Wrappers.handle_mutex_and_catch_runtime
+    @handle_mutex_and_catch_runtime
     def update_name(self, pid, name):
         self.setNameSignal.emit(pid, name)
 
-    @Wrappers.handle_mutex_and_catch_runtime
+    @handle_mutex_and_catch_runtime
     def update_total(self, pid, total):
         self.setTotalSignal.emit(pid, total)
 
@@ -293,9 +371,9 @@ class BarUpdater:
             while True:
                 value = next(iterator)
                 yield value
-                self.mbar.update_value(self.pid, value)
+                self.update_value(value)
         except StopIteration:
-            self.mbar.update_value(self.pid, value)
+            self.update_value(value)
             return value
 
     def update_name(self, name):
@@ -310,7 +388,14 @@ class BarUpdater:
 
 def slow_loop_test(idx, count, sleep_time, pbar: BarUpdater = None):
     for i in pbar(range(count), descr=f'{idx}', total=count):
-        for j in range(int(10000 * sleep_time * 1000)):
+        for j in range(int(30000 * sleep_time * 1000)):
+            k = j + i
+    return idx, count
+
+
+def slow_loop_test2(idx, count, sleep_time, pbar: BarUpdater = None):
+    for i in range(count):
+        for j in range(int(30000 * sleep_time * 1000)):
             k = j + i
     return idx, count
 
@@ -329,8 +414,31 @@ def run_test():
     for name, args in zip(it, it_args):
         rand_str, rand_count, rand_sleep = args
         mbar.add_task(slow_loop_test, (rand_str, rand_count,), {'sleep_time': rand_sleep}, descr=rand_str, total=rand_count)
-    mbar.app.exec()
-    # mbar.begin_processing()
+    # mbar.app.exec()
+    mbar.begin_processing()
+
+
+@wrapped_timer
+def run_test2():
+    it = iter([get_random_string(8, 64) for i in range(num_tasks)])
+    it_args = [[i, get_rand_count(), get_rand_sleep()] for i in copy(it)]
+
+    for name, args in tqdm(zip(it, it_args), total=num_tasks):
+        rand_str, rand_count, rand_sleep = args
+        slow_loop_test2(rand_str, rand_count, **{'sleep_time': rand_sleep})
+
+
+@wrapped_timer
+def run_test3():
+    it = iter([get_random_string(8, 64) for i in range(num_tasks)])
+    it_args = [[i, get_rand_count(), get_rand_sleep()] for i in copy(it)]
+
+    with mp.Pool() as pool:
+        procs = []
+        for name, args in zip(it, it_args):
+            rand_str, rand_count, rand_sleep = args
+            procs.append(pool.apply_async(slow_loop_test2, (rand_str, rand_count, rand_sleep,)))
+        [proc.get() for proc in tqdm(procs, total=num_tasks)]
 
 
 if __name__ == "__main__":
@@ -340,3 +448,5 @@ if __name__ == "__main__":
     get_rand_sleep = lambda: random.randint(1, 5) * 0.001
 
     run_test()
+    # run_test2()
+    # run_test3()
